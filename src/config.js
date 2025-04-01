3f8ab6d312143522302e36c2ff612496ddfd3924
Midlaj <mm4266081@gmail.com>
2025-04-01 21:44:13 +0530

    Vercel test  Commit

--- src/config.js
+++ src/config.js
@@ -1,12 +1,12 @@
 // live
 
-const API_BASE_URL = "https://ecommers-back-end-production.up.railway.app/api";
-const IMG_URL = "https://ecommers-back-end-production.up.railway.app";
+// const API_BASE_URL = "https://ecommers-back-end-production.up.railway.app/api";
+// const IMG_URL = "https://ecommers-back-end-production.up.railway.app";
 
-// local---
+// local
 
-// const API_BASE_URL = "http://192.168.1.11:5000/api";
-// const IMG_URL = "http://192.168.1.11:5000";
+const API_BASE_URL = "http://192.168.1.11:5000/api";
+const IMG_URL = "http://192.168.1.11:5000";
 
 export { API_BASE_URL, IMG_URL };
 export default API_BASE_URL;  // ✅ Added default export
