b4c5e48dbaf348977abdcac9c418da75417dc971
Midlaj <mm4266081@gmail.com>
2025-04-06 22:55:06 +0530

    Header small Update in Router

--- src/components/Logoutbtn.jsx
+++ src/components/Logoutbtn.jsx
@@ -3,18 +3,14 @@ import { useDispatch } from "react-redux";
 import { logout } from "../redux/authSlice";
 import { logoutUser } from "../services/authService";
 import { useSelector } from "react-redux";
-import { useNavigate } from "react-router-dom";
 
 function Logoutbtn() {
-
-  const navigate = useNavigate();
   const dispatch = useDispatch();
     const username =  useSelector((state) => state.auth.username);
   const handleLogout = async () => {
     try {
       const data = await logoutUser(username);
       dispatch(logout());
-      navigate('/')
     } catch (error) {
       console.error("Error:", error.response ? error.response.data : error);
     }
